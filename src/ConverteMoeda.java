d362ea84fa484edc5e73bfde2bf5a044df78ab53
DarkFelipe <fr.fisica@gmail.com>
2023-07-04 19:50:36 -0300

    programa finalizado.

--- src/ConverteMoeda.java
+++ src/ConverteMoeda.java
@@ -5,7 +5,7 @@ public class ConverteMoeda {
     
     public void escolherMoeda(){
         Object[] opcoesMoedas = { "De Reais a Dólares","De Reais a Euros", "De Reais a Libras", "De Reais a peso Argentino", 
-        "De Reais a Peso Chileno", "De Dólares a Reais","De Euros a Reais","De Libras a reais", 
+        "De Reais a Peso Chileno", "De Dólares a Reais","De Euros a Reais","De Libras a Reais", 
         "De Peso Argentino a Reais", "De Peso Chileno a Reais"};
         //criei as opções de moedas.
         //provavelmente terei que fazer um IF ELSE para as outras opções de conversão
@@ -16,6 +16,51 @@ public class ConverteMoeda {
         
         ValidaValor valida = new ValidaValor();
         valida.validarValor();
+
+        ValoresMoeda moeda = new ValoresMoeda();
+
+        if(moedaSelecionada == "De Reais a Dólares"){
+            double realDolar = valida.getValor()/moeda.getDolar();
+            Object str = String.format("%.2f", realDolar);
+            JOptionPane.showMessageDialog(null, "O valor em dólares é: "+ str, "Conversão", 1);
+        } else if (moedaSelecionada == "De Reais a Euros"){
+            double realEuro = valida.getValor()/moeda.getEuro();
+            Object str1 = String.format("%.2f", realEuro);
+            JOptionPane.showMessageDialog(null, "O valor em euros é: "+ str1, "Conversão", 1);
+        } else if (moedaSelecionada == "De Reais a Libras"){
+            double realLibra = valida.getValor()/moeda.getLibra();
+            Object str2 = String.format("%.2f", realLibra);
+            JOptionPane.showMessageDialog(null, "O valor em libras é: "+ str2, "Conversão", 1);
+        } else if (moedaSelecionada == "De Reais a Peso Argentino"){
+            double realArgentino = valida.getValor()/moeda.getPesoArgentino();
+            Object str3 = String.format("%.2f", realArgentino);
+            JOptionPane.showMessageDialog(null, "O valor em pesos argentinos é: "+ str3, "Conversão", 1);
+        } else if (moedaSelecionada == "De Reais a Peso Chileno"){
+            double realChileno = valida.getValor()/moeda.getPesoChileno();
+            Object str4 = String.format("%.2f", realChileno);
+            JOptionPane.showMessageDialog(null, "O valor em pesos chilenos é: "+ str4, "Conversão", 1);
+        } else if (moedaSelecionada == "De Dólares a Reais") {
+            double dolarReal = valida.getValor()*moeda.getDolar();
+            Object str5 = String.format("%.2f", dolarReal);
+            JOptionPane.showMessageDialog(null, "O valor em reais é: "+ str5, "Conversão", 1);
+        } else if (moedaSelecionada == "De Euros a Reais") {
+            double euroReal = valida.getValor()*moeda.getEuro();
+            Object str6 = String.format("%.2f", euroReal);
+            JOptionPane.showMessageDialog(null, "O valor em reais é: "+ str6, "Conversão", 1);
+        } else if (moedaSelecionada == "De Libras a Reais") {
+            double libraReal = valida.getValor()*moeda.getLibra();
+            Object str7 = String.format("%.2f", libraReal);
+            JOptionPane.showMessageDialog(null, "O valor em reais é: "+ str7, "Conversão", 1);
+        } else if (moedaSelecionada == "De Peso Argentino a Reais") {
+            double argentinoReal = valida.getValor()*moeda.getPesoArgentino();
+            Object str8 = String.format("%.2f", argentinoReal);
+            JOptionPane.showMessageDialog(null, "O valor em reais é: "+ str8, "Conversão", 1);
+        }else {
+            double chilenoReal = valida.getValor()*moeda.getPesoChileno();
+            Object str9 = String.format("%.2f", chilenoReal);
+            JOptionPane.showMessageDialog(null, "O valor em reais é: "+ str9, "Conversão", 1);
+        }
+        
     
     }
 
---
+++ src/ValoresMoeda.java
@@ -0,0 +1,54 @@
+public class ValoresMoeda {
+    
+    // valores das moedas em 04/07/2023.
+
+    private double euro=5.26;
+    private double dolar = 4.83;
+    private double pesoChileno=0.0061;
+    private double pesoArgentino=0.02;
+    private double real;
+    private double libra=6.15;
+    
+    public double getEuro() {
+        return euro;
+    }
+    public void setEuro(double euro) {
+        this.euro = euro;
+    }
+    public double getDolar() {
+        return dolar;
+    }
+    public void setDolar(double dolar) {
+        this.dolar = dolar;
+    }
+    public double getPesoChileno() {
+        return pesoChileno;
+    }
+    public void setPesoChileno(double pesoChileno) {
+        this.pesoChileno = pesoChileno;
+    }
+    public double getPesoArgentino() {
+        return pesoArgentino;
+    }
+    public void setPesoArgentino(double pesoArgentino) {
+        this.pesoArgentino = pesoArgentino;
+    }
+    public double getReal() {
+        return real;
+    }
+    public void setReal(double real) {
+        this.real = real;
+    }
+    public double getLibra() {
+        return libra;
+    }
+    public void setLibra(double libra) {
+        this.libra = libra;
+    }
+
+    
+
+
+
+
+}
